2215b786dc7e70bf3a02ab861dca2f4607ac5dc4
Ragnazar <avdeyev.iv@gmail.com>
2022-09-19 11:05:58 +0300

    initial-commit

---
+++ src/main/java/pro/sky/homeworks/homework216/Homework216Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworks.homework216;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework216Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework216Application.class, args);
+    }
+
+}
